eb426c489d5764936b21ad8c188dca37a8ffd690
Junior Martinez <martinezdasilveirajunior@yahoo.com>
2023-01-29 11:09:45 -0300

    more updating

--- dealership-schema.sql
+++ dealership-schema.sql
@@ -17,6 +17,10 @@ CREATE TABLE motorcycle(
     moto_year DATE NOT NULL
 );
 
+CREATE TABLE employees(
+
+)
+
 -- creating a table from another table already created
 CREATE TABLE car_copy AS
 SELECT car_name, car_color, car_year
@@ -61,40 +65,55 @@ VALUES 	('kawasaki', 'green', '2023-02-01'),
 		('hornet', 'white', '2000-05-01')
 ;
 
--- SELECT        
-SELECT * FROM car --returns a limit by 5 results of cars in the car table
+-- SELECT --
+
+--returns a limit by 5 results of cars in the car table
+SELECT * FROM car 
 ORDER BY car_name
 LIMIT 5;
 
+-- returns a counting of all cars that starts with 'c' and orders ascendent by car name
 SELECT count(car_id), car_name
 FROM car
 WHERE car_name LIKE 'c%'
 GROUP BY car_name
 ORDER BY car_name
 
-SELECT moto_name, count(moto_id) -- select that returns a count kawasaki's
+-- select that returns a counting of kawasaki's
+SELECT moto_name, count(moto_id) 
 FROM motorcycle
 WHERE moto_name LIKE 'kawasaki'
 GROUP BY moto_name
 
-SELECT count(moto_id) as "Moto's that start w/ 'k'" -- select that returns a count of moto's that starts with the letter 'k'
+-- select that returns a count of moto's that starts with the letter 'k'
+SELECT count(moto_id) as "Moto's that start w/ 'k'" 
 FROM motorcycle
 WHERE moto_name LIKE 'k%'
 
-
-SELECT * FROM car -- selects all the cars with the name 'tida'
+-- selects all the cars with the name 'tida'
+SELECT * FROM car 
 WHERE car_name LIKE 'tida';
 
-SELECT * FROM car -- returns all cars that starts with the letter 't'
+-- returns all cars that starts with the letter 't'
+SELECT * FROM car 
 WHERE car_name LIKE 't%'; 
 
-SELECT DISTINCT
-	moto.moto_name as moto, 
-	moto.moto_year as "moto year",
-	car.car_name as car,
-	car.car_year as "car year"
-FROM motorcycle as moto, car
-WHERE MONTH(moto.moto_year) and MONTH(car.car_year) LIKE '11';
+-- select all cars that have 3 letters and orders in descendent way by the car_year
+SELECT * FROM car 
+WHERE car_name LIKE '___'
+ORDER BY car_year desc
+
+-- UNION that returns all cars and motos that have been launched on november, excluding the year
+SELECT "MOTO" AS TYPE, moto_name, moto_year
+FROM motorcycle
+WHERE MONTH(moto_year) = 11
+
+UNION
+
+SELECT 'CAR' AS TYPE, car_name, car_year
+FROM car
+WHERE MONTH(car_year) = 11;
+
 
 -- DELETE 
 DELETE FROM car -- delete all cars where the year's equal to 2010-01-01
