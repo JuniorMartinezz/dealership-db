7f090fd118bfb50617e891c649c51177f6b5f57a
Junior Martinez <martinezdasilveirajunior@yahoo.com>
2023-01-28 21:49:19 -0300

    updating dealership

--- dealership-schema.sql
+++ dealership-schema.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE dealership;
+USE dealership;
+
 -- CREATE TABLE
 CREATE TABLE car(
 	car_id INT AUTO_INCREMENT,
@@ -37,21 +40,48 @@ ADD car_year date not null;
 
 -- INSERT
 INSERT INTO car (car_name, car_color, car_year)
-VALUES 	('gol', 'green', '2010-01-01'),
-		('corsa', 'blue', '2010-01-01'),
-		('celta', 'black', '2010-01-01'),
-		('gol', 'red', '2010-01-01'),
-		('onix', 'black', '2010-01-01'),
-		('saveiro', 'green', '2010-01-01'),
-		('fusca', 'brown', '2010-01-01'),
-		('tida', 'white', '2010-01-01');
+VALUES 	('gol', 'green', '2023-02-01'),
+		('corsa', 'blue', '2000-11-02'),
+		('celta', 'black', '2011-12-03'),
+		('gol', 'red', '2022-11-22'),
+		('onix', 'black', '2013-10-09'),
+		('saveiro', 'green', '2010-09-10'),
+		('fusca', 'brown', '2009-02-01'),
+		('tida', 'white', '2000-05-01')
+;
+
+INSERT INTO motorcycle (moto_name, moto_color, moto_year)
+VALUES 	('kawasaki', 'green', '2023-02-01'),
+		('yamaha', 'blue', '2000-11-02'),
+		('bis', 'black', '2011-12-03'),
+		('suzuki', 'red', '2022-11-22'),
+		('kawasaki', 'black', '2013-10-09'),
+		('honda', 'green', '2010-09-10'),
+		('bmw', 'brown', '2009-02-01'),
+		('hornet', 'white', '2000-05-01')
+;
 
 -- SELECT        
-SELECT * FROM car
+SELECT * FROM car --returns a limit by 5 results of cars in the car table
+ORDER BY car_name
+LIMIT 5;
+
+SELECT * FROM car -- selects all the cars with the name 'tida'
 WHERE car_name LIKE 'tida';
 
+SELECT * FROM car -- returns all cars that starts with the letter 't'
+WHERE car_name LIKE 't%'; 
+
+SELECT DISTINCT
+	moto.moto_name as moto, 
+	moto.moto_year as "moto year",
+	car.car_name as car,
+	car.car_year as "car year"
+FROM motorcycle as moto, car
+WHERE MONTH(moto.moto_year) and MONTH(car.car_year) LIKE '11';
+
 -- DELETE 
-DELETE FROM car
-WHERE car_year LIKE '2010-01-01';
+DELETE FROM car -- delete all cars where the year's equal to 2010-01-01
+WHERE car_year LIKE '2010-09-10';
 
 show tables
